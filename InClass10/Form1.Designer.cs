fd6f96c7ed396f734ed9d58f660810b83e301649
enrique tovar <enriquektovar14@gmail.com>
2019-04-10 15:43:44 -0700

    commit

--- InClass10/Form1.Designer.cs
+++ InClass10/Form1.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-1, -1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(433, 449);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Words Ending with \"T\" or \"E\"";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- InClass10/Form1.cs
+++ InClass10/Form1.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//This is my own work
+//Enrque Tovar
 namespace InClass10
 {
     public partial class Form1 : Form
     {
+        public static System.IO.StreamWriter sw;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sw = System.IO.File.CreateText("Sample.dat");
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+
+                string allText = sr.ReadToEnd();
+                string[] words = allText.Split();
+                int count = 0;            
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    string textWord = words[i];
+
+                    for (int v = 0; v != textWord.Length - 1; v++)
+                    {
+                        if (textWord[v] == 't' || textWord[v] == 'e' || textWord[v] == 'T' || textWord[v] == 'E')
+                        {
+                            count++;
+                        }
+                    }                  
+                }
+                richTextBox1.Text += "Words that end with T or E: " + count;
+            }
+            sw.Close();
+        }
+
+        private object tolower(object p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
+
+
+
+
+
